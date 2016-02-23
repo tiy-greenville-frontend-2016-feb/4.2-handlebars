642592ae6107688fe7218b552c2e33ce039b60fb
Dan Dietz <danddietz@gmail.com>
2016-02-23 12:09:15 -0500

    Added handlebar templates.

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,1 +1,77 @@
-console.log("Hello World!");
+var $ = require('jquery');
+var _ = require('underscore');
+var handlebars = require('handlebars');
+
+var albums = [
+  {
+    "title": "Album 1",
+    "thumb": {"title": "Album Art", "image": "http://unsplash.it/75/75"},
+    "photos": [
+      {"title": "Photo 1", "image": "http://unsplash.it/75/75"},
+      {"title": "Photo 2", "image": "http://unsplash.it/75/75"},
+      {"title": "Photo 3", "image": "http://unsplash.it/75/75"},
+      {"title": "Photo 4", "image": "http://unsplash.it/75/75"},
+      {"title": "Photo 5", "image": "http://unsplash.it/75/75"},
+      {"title": "Photo 5", "image": "http://unsplash.it/75/75"}
+    ]
+  },
+  {
+    "title": "Album 2",
+    "thumb": {"title": "Album Art", "image": "http://unsplash.it/75/75"},
+    "photos": [
+      {"title": "Photo 1", "image": "http://unsplash.it/75/75"},
+      {"title": "Photo 2", "image": "http://unsplash.it/75/75"},
+      {"title": "Photo 3", "image": "http://unsplash.it/75/75"},
+      {"title": "Photo 4", "image": "http://unsplash.it/75/75"},
+      {"title": "Photo 5", "image": "http://unsplash.it/75/75"},
+      {"title": "Photo 5", "image": "http://unsplash.it/75/75"}
+    ]
+  },
+  {
+    "title": "Album 3",
+    "thumb": {"title": "Album Art", "image": "http://unsplash.it/75/75"},
+    "photos": [
+      {"title": "Photo 1", "image": "http://unsplash.it/75/75"},
+      {"title": "Photo 2", "image": "http://unsplash.it/75/75"},
+      {"title": "Photo 3", "image": "http://unsplash.it/75/75"},
+      {"title": "Photo 4", "image": "http://unsplash.it/75/75"},
+      {"title": "Photo 5", "image": "http://unsplash.it/75/75"},
+      {"title": "Photo 5", "image": "http://unsplash.it/75/75"}
+    ]
+  },
+  {
+    "title": "Album 4",
+    "thumb": {"title": "Album Art", "image": "http://unsplash.it/75/75"},
+    "photos": [
+      {"title": "Photo 1", "image": "http://unsplash.it/75/75"},
+      {"title": "Photo 2", "image": "http://unsplash.it/75/75"},
+      {"title": "Photo 3", "image": "http://unsplash.it/75/75"},
+      {"title": "Photo 4", "image": "http://unsplash.it/75/75"},
+      {"title": "Photo 5", "image": "http://unsplash.it/75/75"},
+      {"title": "Photo 5", "image": "http://unsplash.it/75/75"}
+    ]
+  }
+];
+
+
+$(function(){
+
+  // Setup my template
+  var totalAlbums = albums.length;
+
+
+  var source   = $("#header-template").html();
+  var template = handlebars.compile(source);
+  var renderedTemplate = template({"count": totalAlbums});
+
+  $('.container').html(renderedTemplate);
+
+  var albumTile = $('#album-tile').html();
+  var albumTileTemplate = handlebars.compile(albumTile);
+
+  context = {'albums': albums};
+
+  var renderedTemplate = albumTileTemplate(context);
+
+  $('.container').append(renderedTemplate);
+});
